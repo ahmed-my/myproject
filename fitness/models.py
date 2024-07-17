4de3e66fe7f912639b10b09eaa73fb2752f6372c
ahmed-my <yahaya.mymohammed@gmail.com>
2024-07-17 02:29:51 +0100

    refactor image upload code

--- fitness/models.py
+++ fitness/models.py
@@ -1,8 +1,22 @@
 from django.db import models
+from tinymce.models import HTMLField
+import bleach
+
+
+# Update allowed tags and attributes
+allowed_tags = [
+    'a', 'abbr', 'acronym', 'b', 'blockquote', 'code', 'em', 'i', 'li', 'ol', 
+    'strong', 'bold', 'ul', 'p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'img', 'span'
+]
+allowed_attrs = {
+    'a': ['href', 'title'],
+    'img': ['src', 'alt', 'width', 'height'],
+    'span': ['style']
+}
 
 # Create your models here.
 class Fitness(models.Model):
-    name = models.TextField()
+    name = models.CharField(max_length=75)
     address = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
     # add this for the image below
@@ -21,8 +35,8 @@ class Image(models.Model):
         return self.title
 
 class Lesson(models.Model):
-    name = models.TextField()
-    body = models.TextField()
+    name = models.CharField(max_length=75)
+    body = HTMLField()
     slug = models.SlugField()
     # add this for the image below
     banner = models.ImageField(default='fallback.png', blank=True)
@@ -34,7 +48,7 @@ class Lesson(models.Model):
 # create table model course
 class Course(models.Model):
     title = models.CharField(max_length=75)
-    body = models.TextField()
+    body = HTMLField()
     slug = models.SlugField()
     banner = models.ImageField(default='fallback.png', blank=True)
 
