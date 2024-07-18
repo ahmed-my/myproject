7f719c88d520ad54482a406ca01228042f3e3585
ahmed-my <yahaya.mymohammed@gmail.com>
2024-07-18 21:31:25 +0100

    packages upgrade installation

--- posts/forms.py
+++ posts/forms.py
@@ -1,8 +1,10 @@
 from django import forms
+from tinymce.widgets import TinyMCE
 from . import models
 
 class CustomForm(forms.ModelForm):
+    body = forms.CharField(widget=TinyMCE(attrs={'cols': 80, 'rows': 30}))
     
     class Meta:
         model = models.Post
-        fields = ("title", "body", "slug", "banner")
+        fields = ("title", "body", "slug", "banner") # Adjust fields as needed
