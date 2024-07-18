34d24907eb0b56784963e5f112744f5062bbe3ed
ahmed-my <yahaya.mymohammed@gmail.com>
2024-07-18 16:04:38 +0100

    adding content to the home page

--- fitness/views.py
+++ fitness/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Fitness, Image, Lesson, Course
+from .models import Fitness, Image, Lesson, Course, Home
 from posts.models import Post
 
 def fitness_items(request):
@@ -13,18 +13,21 @@ def search(request):
         lesson_results = Lesson.objects.filter(name__icontains=query)
         course_results = Course.objects.filter(title__icontains=query)
         post_results = Post.objects.filter(title__icontains=query)
+        home_results = Home.objects.filter(title__icontains=query)
     else:
         fitness_results = Fitness.objects.none()
         lesson_results = Lesson.objects.none()
         course_results = Course.objects.none()
         post_results = Post.objects.none()
+        home_results = Home.objects.none()
 
     context = {
         'query': query,
         'fitness': fitness_results,
         'lessons': lesson_results,
         'courses': course_results,
-        'posts': post_results
+        'posts': post_results,
+        'home': home_results
     }
     return render(request, 'fitness/search_results.html', context)
 
@@ -32,12 +35,14 @@ def lesson_page(request, param):
     posts = Post.objects.all()
     lessons = Lesson.objects.all()
     courses = Course.objects.all()
+    home_content = Home.objects.all()
     lesson = get_object_or_404(Lesson, slug=param)
     context = {
         'posts': posts,
         'lessons': lessons,
         'courses': courses,
-        'lesson': lesson
+        'lesson': lesson,
+        'home_content': home_content
     }
     return render(request, 'fitness/lesson_page.html', context)
 
@@ -45,11 +50,13 @@ def course_page(request, param):
     posts = Post.objects.all()
     courses = Course.objects.all()
     lessons = Lesson.objects.all()
+    home_content = Home.objects.all()
     course = get_object_or_404(Course, slug=param)
     context = {
         'posts': posts,
         'courses': courses,
         'lessons': lessons,
-        'course': course
+        'course': course,
+        'home_content': home_content
     }
     return render(request, 'fitness/course_page.html', context)
