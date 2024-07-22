60cbbb4e06771f3b2b6a6e185bb41ec9cd9dcfae
ahmed-my <yahaya.mymohammed@gmail.com>
2024-07-22 22:19:46 +0100

    email feature completed and working well

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
 from . import views
+from .views import CustomPasswordResetView, CustomPasswordResetConfirmView
+from django.contrib.auth import views as auth_views
 
 app_name = 'users'
 
@@ -10,10 +11,9 @@ urlpatterns = [
     path('logout/', views.logout_user, name='logout_user'),
 
     # Custom Password reset URLs
-    # Password reset URLs
-    path('password_reset/', views.CustomPasswordResetView.as_view(), name='password_reset'),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
+    path('password_reset/', CustomPasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
 ]
