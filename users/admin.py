1ea71ab8cc01dcdd924bb4eb4765bd12faea91e5
ahmed-my <yahaya.mymohammed@gmail.com>
2024-07-26 05:15:28 +0100

    unique user create

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Portfolio
+from .models import UserProfile
 
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'profile_id')  # Display user and profile_id in the admin list view
+    search_fields = ('user__username', 'profile_id')  # Allow searching by username and profile_id
+
+admin.site.register(UserProfile, UserProfileAdmin)
 admin.site.register(Portfolio)
